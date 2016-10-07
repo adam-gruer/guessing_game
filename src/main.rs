c21627a8848d281102a56e831813575cc75b80e0
Adam Gruer <abbot.grover@gmail.com>
2016-10-07 22:18:42 +1100

    Adding code to convert guess to Int32 so it can be compared to secret numer

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,15 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+         .expect("please type a number!");
+
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-      Ordering::Less      => println!("Too small!");
-      Ordering::Greater   => println!("Too big!");
-      Ordering::Equal     => println!("You win!");
+      Ordering::Less      => println!("Too small!"),
+      Ordering::Greater   => println!("Too big!"),
+      Ordering::Equal     => println!("You win!")
 
     }
 }
